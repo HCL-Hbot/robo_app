6257c3d4c996013c7b6bdddf70944c78c3843e2f
Richard <kroesen.ri@gmail.com>
2024-05-29 14:06:38 +0200

    Add serial driver example.

--- Client/main.cpp
+++ Client/main.cpp
@@ -1,92 +1,34 @@
-#include <math.h>
-#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include "wake_word_lib_hooks.h"
-#include <string>
-#include <iostream>
-#include <fstream>
-#include <rtp_streamer.hpp>
+#include <string.h>
 
-const char *library_path = "../wake_word_lib/porcupine/libpv_porcupine.so";
-const char *api_key = "***REMOVED***";
-const char *model_path = "../wake_word_lib/porcupine_params.pv";
-const char *keyword_path = "../wake_word_lib/model/hey_quiri_x86_64.ppn";
+#include <serial_driver.hpp>
 
+int main() {
+    SerialPort serial(PORT_NAME, BAUD_RATE);
 
-static volatile bool is_interrupted = false;
-
-void interrupt_handler(int _) {
-    (void) _;
-    is_interrupted = true;
-}
-
-void start_mic_capture_stream(pv_recorder_t* rec_inst){
-    fprintf(stdout, "Start recording...\n");
-    pv_recorder_status_t recorder_status = pv_recorder_start(rec_inst);
-    if (recorder_status != PV_RECORDER_STATUS_SUCCESS) {
-        fprintf(stderr, "Failed to start device with %s.\n", pv_recorder_status_to_string(recorder_status));
-        exit(1);
+    // List available ports
+    struct sp_port** ports;
+    sp_list_ports(&ports);
+    for (int i = 0; ports[i]; i++) {
+        std::cout << "Found port: " << sp_get_port_name(ports[i]) << std::endl;
     }
-}
+    sp_free_port_list(ports);
 
-void get_samples_from_mic(pv_recorder_t* rec_inst, int16_t* pcm) {
-        pv_recorder_status_t recorder_status = pv_recorder_read(rec_inst, pcm);
-        if (recorder_status != PV_RECORDER_STATUS_SUCCESS)
-        {
-            fprintf(stderr, "Failed to read with %s.\n", pv_recorder_status_to_string(recorder_status));
-            exit(1);
-        }
-}
+    try {
+        serial.open();
+        std::string data = "Hello, World 2!";
+        std::cout << "Sending data: " << data << std::endl;
+        serial.write(data);
+        std::cout << "Reading data..." << std::endl;
+        std::string received_data = serial.read();
+        std::cout << "Received data: " << received_data << std::endl;
 
-void writePCMToFile(const std::string& filename, const int16_t* pcmData, size_t dataSize, bool append = false) {
-    std::ios_base::openmode mode = std::ios::binary;
-    if (append) {
-        mode |= std::ios::app;
-    } else {
-        mode |= std::ios::out;
+        serial.close();
+    } catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
     }
 
-    std::ofstream outFile(filename, mode);  // Open file in binary mode, with append option if specified
-    if (!outFile) {
-        std::cerr << "Failed to open file for writing: " << filename << std::endl;
-        return;
-    }
-
-    // Write PCM data to file
-    outFile.write(reinterpret_cast<const char*>(pcmData), dataSize * sizeof(int16_t));
-    if (!outFile) {
-        std::cerr << "Failed to write data to file: " << filename << std::endl;
-    }
-
-    outFile.close();  // Close the file
-}
-
-int main(int argc, char *argv[]) {
-    signal(SIGINT, interrupt_handler);
-    wake_word_lib wake_word_detector(-1, library_path, api_key, model_path, keyword_path);
-    RtpStreamer rtp("127.0.0.1", 5004, 512, 48000);
-
-    start_mic_capture_stream(wake_word_detector.get_recorder_inst());
-
-    uint32_t frame_length = wake_word_detector.get_frame_length();
-    int16_t *pcm = (int16_t*)malloc(frame_length * sizeof(int16_t));
-    printf("%d\n", frame_length);
-    if (!pcm) {
-        fprintf(stderr, "Failed to allocate pcm memory.\n");
-        exit(1);
-    }
-    
-    pv_recorder_t *recorder = wake_word_detector.get_recorder_inst();
-    while(!is_interrupted) {
-        get_samples_from_mic(recorder, pcm);
-        int wake_word_detected = wake_word_detector.detect_wakeword(pcm);
-        if(wake_word_detected) {
-            printf("Wake word detected!\n");
-            rtp.start();
-        }
-    }
-    free(pcm);
-    pv_recorder_delete(recorder);
-    return 0;
+    return EXIT_SUCCESS;
 }
