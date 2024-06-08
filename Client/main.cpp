5b4e11c4f100f95270803cd9c4e47fd09d10cb28
RPI-ROBO <kroesen.ri@gmail.com>
2024-06-08 18:46:56 +0200

    Enh tested on the RPI, seems to be working fine.

--- Client/main.cpp
+++ Client/main.cpp
@@ -4,12 +4,12 @@
 #include <brainboard_host.hpp>
 
 int main() {
-    BRAINBOARD_HOST::DeviceController controller("/dev/ttyUSB0", 115200);
+    BRAINBOARD_HOST::DeviceController controller("/dev/ttyS0", 115200);
 
     //controller.controlEye(BRAINBOARD_HOST::EyeID::LEFT, 100, 100, BRAINBOARD_HOST::EyeAnimation::MOVE);
-    //controller.controlEye(BRAINBOARD_HOST::EyeID::RIGHT, 100, 100, BRAINBOARD_HOST::EyeAnimation::BLINK_ANIM, 2000);
-    // controller.controlHead(BRAINBOARD_HOST::MotorID::AZIMUTH, 50, 100);
-    // controller.controlHead(BRAINBOARD_HOST::MotorID::ELEVATION, 10, 100);
-    controller.setColor(BRAINBOARD_HOST::LedID::HEAD, BRAINBOARD_HOST::Color::WHITE);
+    // controller.controlEye(BRAINBOARD_HOST::EyeID::RIGHT, 100, 100, BRAINBOARD_HOST::EyeAnimation::BLINK_ANIM, 2000);
+    controller.controlHeadMovement(BRAINBOARD_HOST::MotorID::AZIMUTH, 50, 100);
+    controller.controlHeadMovement(BRAINBOARD_HOST::MotorID::ELEVATION, 10, 100);
+    controller.setColor(BRAINBOARD_HOST::LedID::BODY, BRAINBOARD_HOST::Color::BLUE);
     return 0;
 }
--- Client/nervousPico_connectivity/serial_settings.hpp
+++ Client/nervousPico_connectivity/serial_settings.hpp
@@ -5,6 +5,6 @@
 
 namespace SERIAL {
     static constexpr uint32_t BAUD_RATE = 115200;   
-    static const char* PORT_NAME = "/dev/ttyUSB0";
+    static const char* PORT_NAME = "/dev/serial0";
 } // namespace SERIAL
 #endif // SERIAL_SETTINGS_HPP
