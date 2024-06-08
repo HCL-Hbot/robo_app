eb9a8b7929590a1f82a44e5e5e97356d9d125d80
Richard <kroesen.ri@gmail.com>
2024-06-08 17:52:55 +0200

    Enh quick tested the current functionality, not happy about how it is implemented. One mess, but this is enough for proto.

--- Client/main.cpp
+++ Client/main.cpp
@@ -1,34 +1,15 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-#include <serial_driver.hpp>
+#include <chrono>
+#include <thread>
+#include <iostream>
+#include <brainboard_host.hpp>
 
 int main() {
-    SerialPort serial(PORT_NAME, BAUD_RATE);
+    BRAINBOARD_HOST::DeviceController controller("/dev/ttyUSB0", 115200);
 
-    // List available ports
-    struct sp_port** ports;
-    sp_list_ports(&ports);
-    for (int i = 0; ports[i]; i++) {
-        std::cout << "Found port: " << sp_get_port_name(ports[i]) << std::endl;
-    }
-    sp_free_port_list(ports);
-
-    try {
-        serial.open();
-        std::string data = "Hello, World 2!";
-        std::cout << "Sending data: " << data << std::endl;
-        serial.write(data);
-        std::cout << "Reading data..." << std::endl;
-        std::string received_data = serial.read();
-        std::cout << "Received data: " << received_data << std::endl;
-
-        serial.close();
-    } catch (const std::exception& e) {
-        std::cerr << e.what() << std::endl;
-        return EXIT_FAILURE;
-    }
-
-    return EXIT_SUCCESS;
+    //controller.controlEye(BRAINBOARD_HOST::EyeID::LEFT, 100, 100, BRAINBOARD_HOST::EyeAnimation::MOVE);
+    //controller.controlEye(BRAINBOARD_HOST::EyeID::RIGHT, 100, 100, BRAINBOARD_HOST::EyeAnimation::BLINK_ANIM, 2000);
+    // controller.controlHead(BRAINBOARD_HOST::MotorID::AZIMUTH, 50, 100);
+    // controller.controlHead(BRAINBOARD_HOST::MotorID::ELEVATION, 10, 100);
+    controller.setColor(BRAINBOARD_HOST::LedID::HEAD, BRAINBOARD_HOST::Color::WHITE);
+    return 0;
 }
--- Client/nervousPico_connectivity/brainboard_refs.hpp
+++ Client/nervousPico_connectivity/brainboard_refs.hpp
@@ -3,7 +3,7 @@
 
 #include <stdint.h>
 
-namespace SERIAL {
+namespace BRAINBOARD_HOST {
 
 /* EYES REFS: */
     enum class EyeAnimation : uint8_t { 
@@ -15,14 +15,14 @@ namespace SERIAL {
     };
 
     enum class EyeID : uint8_t {
-        LEFT        = 0,
-        RIGHT       = 1,
+        LEFT        = 1,
+        RIGHT       = 0,
         BOTH        = 2,
     }; 
 
 /* LEDSTRIP REF: */
     enum class LedstripCommandTypes {
-        SET_RGB             = 0,
+        SET_COLOR           = 0,
         ANIM_BLINK          = 1,
         ANIM_FADE_OUT       = 2,
         ANIM_FADE_IN        = 3,
@@ -34,10 +34,18 @@ namespace SERIAL {
         // BOTH        = 2,
     }; 
 
+    enum class Color {
+        RED,
+        GREEN,
+        BLUE,
+        WHITE,
+        BLACK
+    };
+
 /* Motor REF */
     enum class MotorID : uint8_t {
-        AZIMUTH        = 0, // X axis
-        ELEVATION      = 1, // Y axis
+        AZIMUTH        = 1, // X axis
+        ELEVATION      = 2, // Y axis
         // BOTH        = 2,
     }; 
 
