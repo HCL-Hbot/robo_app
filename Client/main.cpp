171a29cb57b9d4e0d02eabaea6f09c8a8a527601
Victor Hogeweij <vo.hogeweij@student.han.nl>
2024-06-11 20:42:01 +0200

    -add rtp receiver and new wakeword lib

--- Client/main.cpp
+++ Client/main.cpp
@@ -2,7 +2,7 @@
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include "wake_word_lib_hooks.h"
+// #include "wake_word_lib_hooks.h"
 #include <string>
 #include <iostream>
 #include <fstream>
@@ -10,12 +10,13 @@
 #include <atomic>
 #include <mosquitto.h>
 #include "model/model.h"
+#include <openwakeword.hpp>
 
-#ifdef RASPBERRY_PI
-const char *library_path = "../wake_word_lib/porcupine/libpv_porcupine_rpi4.so";
-#else
-const char *library_path = "../wake_word_lib/porcupine/libpv_porcupine.so";
-#endif
+// #ifdef RASPBERRY_PI
+// const char *library_path = "../wake_word_lib/porcupine/libpv_porcupine_rpi4.so";
+// #else
+// const char *library_path = "../wake_word_lib/porcupine/libpv_porcupine.so";
+// #endif
 
 
 std::atomic_bool stop_listening = false;
@@ -38,23 +39,23 @@ void interrupt_handler(int _) {
     is_interrupted = true;
 }
 
-void start_mic_capture_stream(pv_recorder_t* rec_inst){
-    fprintf(stdout, "Start recording...\n");
-    pv_recorder_status_t recorder_status = pv_recorder_start(rec_inst);
-    if (recorder_status != PV_RECORDER_STATUS_SUCCESS) {
-        fprintf(stderr, "Failed to start device with %s.\n", pv_recorder_status_to_string(recorder_status));
-        exit(1);
-    }
-}
+// void start_mic_capture_stream(pv_recorder_t* rec_inst){
+//     fprintf(stdout, "Start recording...\n");
+//     pv_recorder_status_t recorder_status = pv_recorder_start(rec_inst);
+//     if (recorder_status != PV_RECORDER_STATUS_SUCCESS) {
+//         fprintf(stderr, "Failed to start device with %s.\n", pv_recorder_status_to_string(recorder_status));
+//         exit(1);
+//     }
+// }
 
-void get_samples_from_mic(pv_recorder_t* rec_inst, int16_t* pcm) {
-        pv_recorder_status_t recorder_status = pv_recorder_read(rec_inst, pcm);
-        if (recorder_status != PV_RECORDER_STATUS_SUCCESS)
-        {
-            fprintf(stderr, "Failed to read with %s.\n", pv_recorder_status_to_string(recorder_status));
-            exit(1);
-        }
-}
+// void get_samples_from_mic(pv_recorder_t* rec_inst, int16_t* pcm) {
+//         pv_recorder_status_t recorder_status = pv_recorder_read(rec_inst, pcm);
+//         if (recorder_status != PV_RECORDER_STATUS_SUCCESS)
+//         {
+//             fprintf(stderr, "Failed to read with %s.\n", pv_recorder_status_to_string(recorder_status));
+//             exit(1);
+//         }
+// }
 
 void writePCMToFile(const std::string& filename, const int16_t* pcmData, size_t dataSize, bool append = false) {
     std::ios_base::openmode mode = std::ios::binary;
@@ -81,7 +82,7 @@ void writePCMToFile(const std::string& filename, const int16_t* pcmData, size_t 
 
 int main(int argc, char *argv[]) {
     signal(SIGINT, interrupt_handler);
-    wake_word_lib wake_word_detector(-1, library_path, api_key, model_path, keyword_path);
+    // wake_word_lib wake_word_detector(-1, library_path, api_key, model_path, keyword_path);
     RtpStreamer rtp("127.0.0.1", 5004, 512, 48000);
 
     mosquitto_lib_init();
@@ -102,34 +103,34 @@ int main(int argc, char *argv[]) {
 
     mosquitto_subscribe(mosq, nullptr, mqtt_topic.c_str(), 0);
     mosquitto_loop_start(mosq);
+    openwakeword_detector("models/hey_robo.onnx");
 
+    // start_mic_capture_stream(wake_word_detector.get_recorder_inst());
 
-    start_mic_capture_stream(wake_word_detector.get_recorder_inst());
-
-    uint32_t frame_length = wake_word_detector.get_frame_length();
-    int16_t *pcm = (int16_t*)malloc(frame_length * sizeof(int16_t));
-    printf("%d\n", frame_length);
-    if (!pcm) {
-        fprintf(stderr, "Failed to allocate pcm memory.\n");
-        exit(1);
-    }
+    // uint32_t frame_length = wake_word_detector.get_frame_length();
+    // int16_t *pcm = (int16_t*)malloc(frame_length * sizeof(int16_t));
+    // printf("%d\n", frame_length);
+    // if (!pcm) {
+    //     fprintf(stderr, "Failed to allocate pcm memory.\n");
+    //     exit(1);
+    // }
     
-    pv_recorder_t *recorder = wake_word_detector.get_recorder_inst();
+    // pv_recorder_t *recorder = wake_word_detector.get_recorder_inst();
     while(!is_interrupted) {
-        get_samples_from_mic(recorder, pcm);
-        int wake_word_detected = wake_word_detector.detect_wakeword(pcm);
-        if(wake_word_detected) {
-            printf("Wake word detected!\n");
-            rtp.start();
-        }
-        if(stop_listening) {
-            printf("Stop command received!\n");
-            rtp.stop();
-            stop_listening = false;
-        }
+        // get_samples_from_mic(recorder, pcm);
+        // int wake_word_detected = wake_word_detector.detect_wakeword(pcm);
+        // if(wake_word_detected) {
+        //     printf("Wake word detected!\n");
+        //     rtp.start();
+        // }
+        // if(stop_listening) {
+        //     printf("Stop command received!\n");
+        //     rtp.stop();
+        //     stop_listening = false;
+        // }
     }
-    free(pcm);
-    pv_recorder_delete(recorder);
+    // free(pcm);
+    // pv_recorder_delete(recorder);
     mosquitto_loop_stop(mosq, true);
     mosquitto_disconnect(mosq);
     mosquitto_destroy(mosq);
---
+++ Client/rtp_receiver/rtp_receiver.cpp
@@ -0,0 +1,88 @@
+#include <rtp_receiver.hpp>
+#include <iostream>
+#include <fstream>
+
+
+RtpReceiver::RtpReceiver(int port) {
+     gst_init(nullptr, nullptr);
+    loop = g_main_loop_new(nullptr, FALSE);
+
+    pipeline = gst_pipeline_new("rtp-receiver");
+    udpsrc = gst_element_factory_make("udpsrc", "source");
+    rtpdepay = gst_element_factory_make("rtpL16depay", "depay");
+    capsfilter = gst_element_factory_make("capsfilter", "filter");
+    convert = gst_element_factory_make("audioconvert", "convert");
+    resample = gst_element_factory_make("audioresample", "resample");
+    sink = gst_element_factory_make("autoaudiosink", "sink");
+
+    if (!pipeline || !udpsrc || !rtpdepay || !capsfilter || !convert || !resample || !sink) {
+        g_printerr("Failed to create elements.\n");
+        return;
+    }
+
+    g_object_set(udpsrc, "port", port, NULL);
+    
+    GstCaps *caps = gst_caps_new_simple("application/x-rtp",
+                                        "media", G_TYPE_STRING, "audio",
+                                        "clock-rate", G_TYPE_INT, 22050,
+                                        "encoding-name", G_TYPE_STRING, "L16",
+                                        "payload", G_TYPE_INT, 96,
+                                        NULL);
+    g_object_set(capsfilter, "caps", caps, NULL);
+    gst_caps_unref(caps);
+
+    gst_bin_add_many(GST_BIN(pipeline), udpsrc, rtpdepay, capsfilter, convert, resample, sink, NULL);
+    if (!gst_element_link_many(udpsrc, rtpdepay, capsfilter, convert, resample, sink, NULL)) {
+        g_printerr("Failed to link elements.\n");
+        return;
+    }
+
+    bus = gst_element_get_bus(pipeline);
+    gst_bus_add_signal_watch(bus);
+    g_signal_connect(bus, "message::error", G_CALLBACK(+[](GstBus *bus, GstMessage *msg, gpointer user_data) {
+        GError *err;
+        gchar *debug_info;
+        gst_message_parse_error(msg, &err, &debug_info);
+        g_printerr("Error received from element %s: %s\n", GST_OBJECT_NAME(msg->src), err->message);
+        g_printerr("Debugging information: %s\n", debug_info ? debug_info : "none");
+        g_clear_error(&err);
+        g_free(debug_info);
+        g_main_loop_quit((GMainLoop *)user_data);
+    }), loop);
+
+    g_signal_connect(bus, "message::eos", G_CALLBACK(+[](GstBus *bus, GstMessage *msg, gpointer user_data) {
+        g_main_loop_quit((GMainLoop *)user_data);
+    }), loop);
+}
+
+RtpReceiver::~RtpReceiver() {
+    gst_element_set_state(pipeline, GST_STATE_NULL);
+    gst_object_unref(GST_OBJECT(pipeline));
+    g_main_loop_unref(loop);
+}
+
+bool RtpReceiver::isReceivingSamples() {
+    GstState state;
+    GstState pending;
+    GstStateChangeReturn ret = gst_element_get_state(pipeline, &state, &pending, GST_CLOCK_TIME_NONE);
+    return (ret == GST_STATE_CHANGE_SUCCESS || ret == GST_STATE_CHANGE_ASYNC) &&
+           (state == GST_STATE_PLAYING || state == GST_STATE_PAUSED);
+}
+
+void RtpReceiver::pause() {
+    GstStateChangeReturn ret = gst_element_set_state(pipeline, GST_STATE_PAUSED);
+    if (ret == GST_STATE_CHANGE_FAILURE) {
+        g_printerr("Failed to pause the GStreamer pipeline\n");
+    } else {
+        g_print("Pipeline paused\n");
+    }
+}
+
+void RtpReceiver::resume() {
+    GstStateChangeReturn ret = gst_element_set_state(pipeline, GST_STATE_PLAYING);
+    if (ret == GST_STATE_CHANGE_FAILURE) {
+        g_printerr("Failed to resume the GStreamer pipeline\n");
+    } else {
+        g_print("Pipeline resumed\n");
+    }
+}
