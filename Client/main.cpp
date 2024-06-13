4847eb716ef36d50b85dd310fa37dee3b10e8fee
Victor Hogeweij <hogeweyv@gmail.com>
2024-06-13 11:28:02 +0200

    -enh improve rtp_stream functionality

--- Client/main.cpp
+++ Client/main.cpp
@@ -36,11 +36,22 @@ void on_message(struct mosquitto *mosq, void *obj, const struct mosquitto_messag
 
 static volatile bool is_interrupted = false;
 
+
 void interrupt_handler(int _)
 {
     (void)_;
     is_interrupted = true;
 }
+
+void loadEchoCancelModule() {
+    int result = std::system("pactl load-module module-echo-cancel");
+    if (result != 0) {
+        std::cerr << "Failed to load module-echo-cancel." << std::endl;
+    } else {
+        std::cout << "module-echo-cancel loaded successfully." << std::endl;
+    }
+}
+
 
 int main(int argc, char *argv[])
 {
@@ -48,7 +59,7 @@ int main(int argc, char *argv[])
     RtpStreamer rtp("100.72.27.109", 5004, 512, 48000);
     RtpReceiver rtprecv(5002);
     mosquitto_lib_init();
-
+    // loadEchoCancelModule();
     mosquitto *mosq = mosquitto_new(nullptr, true, nullptr);
     if (!mosq)
     {
@@ -78,26 +89,24 @@ int main(int argc, char *argv[])
         if (wake_word_detected)
         {
             printf("Wake word detected!\n");
+            
             rtp.start();
 
         }
         if (stop_listening)
         {
             printf("Stop command received!\n");
-            rtprecv.pause();
+            rtp.stop();
             stop_listening = false;
         }
-        while(rtprecv.isReceivingSamples()) {
-            printf("Receiving samples! waiting!, %d\n", rtprecv.isReceivingSamples());
-            rtp.stop();
-            std::this_thread::sleep_for(std::chrono::milliseconds(100));
-            mic_stream_paused = true;
-        }
-        if(mic_stream_paused) {
-            mic_stream_paused = false;
-            rtp.start();
-        }
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        // while(rtprecv.isReceivingSamples()) {
+        //     printf("Receiving samples! waiting!, %d\n", rtprecv.isReceivingSamples());
+        //     std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        //     mic_stream_paused = true;
+        // }
+        // if(mic_stream_paused) {
+        //     mic_stream_paused = false;
+        // }
     }
     return 0;
 }
--- Client/run_client.sh
+++
@@ -1,3 +0,0 @@
-gst-launch-1.0 udpsrc port=5002 caps="application/x-rtp, media=(string)audio, encoding-name=(string)L16, clock-rate=(int)22050, channels=(int)1" buffer-size=500000 ! rtpjitterbuffer latency=100 ! rtpL16depay ! audioconvert ! audioresample ! autoaudiosink &
-./build/porcupine_demo_mic
-
